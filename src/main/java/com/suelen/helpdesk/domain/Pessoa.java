e6b97a91c5b1f18af55f866022bc4338fdb3e24e
suelenfeijo <suelenfeijobr@gmail.com>
2023-12-26 01:59:31 -0300

    adicionando domain perfil , cliente, pessoa, tecnico

---
+++ src/main/java/com/suelen/helpdesk/domain/Pessoa.java
@@ -0,0 +1,139 @@
+package com.suelen.helpdesk.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.suelen.helpdesk.domain.enums.Perfil;
+
+@Entity
+// a classe pessoa não poderá ser instânciada por que é abstrata, apenas pode ser herdade
+public abstract class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY )
+	protected Integer id;
+	protected String nome;
+	
+	//Faz todo o cálculo para validar se é um cpf
+	@CPF
+	@Column (unique = true)
+	protected String cpf;
+	
+	@Column (unique = true)
+	protected String email;
+	protected String senha;
+	
+	@ElementCollection (fetch = FetchType.EAGER)
+	@CollectionTable(name = "PERFIS")
+	// o new Hash faz uma verificação para evitar de nullpoint exception
+	// e o Set não permite dois perfis de valores iguais
+	protected Set<Integer> perfis = new HashSet<>();
+
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	LocalDate data =  LocalDate.now();
+	
+
+		
+	public Pessoa() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+
+	}
+	
+	
+	public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.senha = senha;
+		addPerfil(Perfil.CLIENTE);
+
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmaill( String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Set<Perfil> getPerfis() {
+		//transformando um perfil 
+		return perfis.stream().map( x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+	}
+	//não recebe uma lista, e apenas um perfil.
+	public void addPerfil(Perfil perfil) {
+		this.perfis.add (perfil.getCodigo());
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	
+		
+//	o hashCode equals faz comparação de um objeto pelo valor dos atributos, não do valor em memória
+	
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+	
+	
+}
---
+++ src/main/java/com/suelen/helpdesk/domain/Tecnico.java
@@ -0,0 +1,58 @@
+package com.suelen.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.suelen.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa {
+	
+	@CollectionTable(name = "PERFIS")
+	protected Set<Integer> perfis = new HashSet<>();
+	
+	private static final long serialVersionUID = 1L;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha );
+	}
+	
+	/*public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.data = obj.getData();
+		addPerfil(Perfil.CLIENTE);
+
+	}
+*/
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+
+	
+}
