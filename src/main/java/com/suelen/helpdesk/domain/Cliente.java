a5d69d6dc86f6f5f8926b87d9763551ef44450e9
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-01 03:16:45 -0300

    crud cliente

--- src/main/java/com/suelen/helpdesk/domain/Cliente.java
+++ src/main/java/com/suelen/helpdesk/domain/Cliente.java
@@ -11,14 +11,13 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.suelen.helpdesk.domain.dtos.ClienteDTO;
 import com.suelen.helpdesk.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
@@ -34,7 +33,7 @@ public class Cliente extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
-	/*public Cliente(ClienteDTO obj) {
+	public Cliente(ClienteDTO obj) {
 		super();
 		this.id = obj.getId();
 		this.nome = obj.getNome();
@@ -42,8 +41,8 @@ public class Cliente extends Pessoa {
 		this.email = obj.getEmail();
 		this.senha = obj.getSenha();
 		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
-		this.data = obj.getData();
-	}*/
+		this.dataCriacao = obj.getDataCriacao();
+	}
 
 	public List<Chamado> getChamados() {
 		return chamados;
--- src/main/java/com/suelen/helpdesk/services/ClienteService.java
+++ src/main/java/com/suelen/helpdesk/services/ClienteService.java
@@ -1,5 +1,80 @@
 package com.suelen.helpdesk.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.suelen.helpdesk.domain.Cliente;
+import com.suelen.helpdesk.domain.Pessoa;
+import com.suelen.helpdesk.domain.dtos.ClienteDTO;
+import com.suelen.helpdesk.repositories.ClienteRepository;
+import com.suelen.helpdesk.repositories.PessoaRepository;
+import com.suelen.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.suelen.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
 public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+/*	@Autowired
+	private BCryptPasswordEncoder encoder;*/
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+
+	public Cliente create(ClienteDTO objDTO) {
+		objDTO.setId(null);
+	//	objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		validaPorCpfEEmail(objDTO);
+		Cliente newObj = new Cliente(objDTO);
+		return repository.save(newObj);
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		
+	/*	if(!objDTO.getSenha().equals(oldObj.getSenha())) 
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));*/
+		
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+		}
+
+		repository.deleteById(id);
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
 
 }
