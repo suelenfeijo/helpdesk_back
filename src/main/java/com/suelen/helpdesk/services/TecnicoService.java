7d246dd7655c45c1eafd65139645d3a4f673ba41
suelenfeijo <suelenfeijobr@gmail.com>
2024-01-01 03:00:32 -0300

    endpoint delete tecnico

--- src/main/java/com/suelen/helpdesk/services/TecnicoService.java
+++ src/main/java/com/suelen/helpdesk/services/TecnicoService.java
@@ -63,6 +63,16 @@ public class TecnicoService {
 		return repository.save(oldObj);
 	}
 	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+
+		repository.deleteById(id);
+	}
+	
 	
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
